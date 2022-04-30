841652bff948ae38a39b9106828e16db9553c925
xjz17 <67282793+xjz17@users.noreply.github.com>
2022-04-30 16:35:31 +0800

    update baos

--- CSSC_compression_code/CSSC_compression_code/ByteArrayOutputStream.cpp
+++ CSSC_compression_code/CSSC_compression_code/ByteArrayOutputStream.cpp
@@ -1,7 +1,67 @@
 #include "ByteArrayOutputStream.h"
-	
-void write(int b){
-   
+// 基础类型到容器转换
+template <typename T>
+std::vector<std::uint8_t> ToByte(T input)
+{
+	std::uint8_t* bytePointer = reinterpret_cast<std::uint8_t*>(&input);
+	return std::vector<std::uint8_t>(bytePointer, bytePointer + sizeof(T));
 }
-void write(long long b);
-void writeBytes(char b[]);
+
+// 容器到基础类型转换
+template <typename T>
+std::vector<std::uint8_t>& operator>>(std::vector<std::uint8_t>& in, T& out)
+{
+	if (in.size() >= sizeof(T)) {
+		out = *reinterpret_cast<T*>(in.data());
+		in.erase(in.begin(), in.begin() + sizeof(T));
+	}else {
+		out = T{ 0 };
+	}
+	return in;
+}
+
+void ByteArrayOutputStream::write(int b){
+   std::vector<std::uint8_t> bytes_tmp = ToByte(b);
+   this->bytes.insert(this->bytes.end(),bytes_tmp.begin(),bytes_tmp.end());
+
+}
+void ByteArrayOutputStream::write(long long b){
+   std::vector<std::uint8_t> bytes_tmp = ToByte(b);
+   this->bytes.insert(this->bytes.end(),bytes_tmp.begin(),bytes_tmp.end());
+}
+void ByteArrayOutputStream::writeBytes(char b[]){
+	int len = sizeof(b);
+	for (int i=0; i < len;i++) {
+		std::vector<std::uint8_t> byte_tmp = ToByte(b[i]);
+        this->bytes.insert(this->bytes.end(), byte_tmp.begin(), byte_tmp.end());
+    }
+}
+
+void ByteArrayOutputStream::write(char b[], int offset, int len)
+{
+	int length = sizeof(b);
+	int end = offset + len;
+	if (end > length || offset < 0) {
+		std::cout << "offset error" << std::endl;
+	}
+	for (int i=offset; i < end; i++) {
+		std::vector<std::uint8_t> byte_tmp = ToByte(b[i]);
+		this->bytes.insert(this->bytes.end(), byte_tmp.begin(), byte_tmp.end());
+	}
+}
+
+void ByteArrayOutputStream::write2file()
+{
+	ofstream outfile;
+	outfile.open(filepath, ios::app);
+	if (!outfile) //检查文件是否正常打开//不是用于检查文件是否存在
+	{
+		cout << "the file can't open" << endl;
+		abort(); //打开失败，结束程序
+	}
+	else
+	{
+		outfile << buf2 << " :world1" << endl;
+		outfile.close();
+	}
+}
