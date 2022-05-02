4597d6c8cb84567389ca147c08aa52924352943f
huangyuxiang03 <2665315699@qq.com>
2022-05-02 18:38:49 +0800

    Revert "Merge branch 'main' of https://github.com/huangyuxiang03/CSSC_compression"
    This reverts commit 11a24994d744321da845ab41a606526f080db3e1, reversing
    changes made to b76d1921429e416faebfacb3a564307f76bd887c.

--- CSSC_compression_code/CSSC_compression_code/main.cpp
+++ CSSC_compression_code/CSSC_compression_code/main.cpp
@@ -6,26 +6,25 @@
 #include <sstream>
 using namespace std;
 
-
-template <typename T>
-void read_csv(string filename,vector<vector<T>> strArray,char seq)
+void read_csv(string filename)
 {
     ifstream inFile(filename);
     string lineStr;
+    vector<vector<long long>> strArray;
     cout << "the whole line is: " << endl;
     int i = 0;
     while (getline(inFile, lineStr))
     {
-        //cout << lineStr << endl;
+        cout << lineStr << endl;
         stringstream ss(lineStr);
         string str;
-        T str2long;
-        vector<T> lineArray;
-        while (getline(ss, str, seq))
+        long str2long;
+        vector<long long> lineArray;
+        while (getline(ss, str, ','))
         {
             stringstream strIn;
             strIn << str;
-            T llNum;
+            long long llNum;
             strIn >> std::hex >> llNum;
             lineArray.push_back(llNum);
         }
@@ -35,22 +34,20 @@ void read_csv(string filename,vector<vector<T>> strArray,char seq)
     }
     
 
-   cout << "--------------------------------------------" << endl;
-    cout << "print what we have saved in the vector: " << endl;
-    for (auto s : strArray)
-    {
-        for (auto x : s)
-        {
-            cout << x << " ";
-        }
-        cout << endl;
-    }
+   //cout << "--------------------------------------------" << endl;
+   // cout << "print what we have saved in the vector: " << endl;
+   // for (auto s : strArray)
+   // {
+   //     for (auto x : s)
+   //     {
+   //         cout << x << " ";
+   //     }
+   //     cout << endl;
+   // }
 
 }
 int main() {
-    vector<vector<long long>> strArrayll;
-    vector<vector<float>> strArrayf;
-    read_csv("C:\\Users\\xiaoj\\Desktop\\data_well_public.hxv", strArrayll,',');
-    read_csv("C:\\Users\\xiaoj\\Desktop\\shore_public.dat", strArrayf, ' ');
-    return 0;
+
+    read_csv("C:\\Users\\xiaoj\\Desktop\\data_well_public.hxv");
+	return 0;
 }
