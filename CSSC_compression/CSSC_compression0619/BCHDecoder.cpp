f8779f97b7b78e497cecb75a4659fd8d3ade21fb
lr-tsinghua11 <lu-r21@mails.tsinghua.edu.cn>
2022-07-03 21:13:23 +0800

    BCH annotation

--- CSSC_compression/CSSC_compression0619/BCHDecoder.cpp
+++ CSSC_compression/CSSC_compression0619/BCHDecoder.cpp
@@ -1,12 +1,28 @@
 #include "BCHDecoder.h"
-
+/**
+ * @brief according to the status to get last 12 bits by BCH algorithm.
+ * @param ss
+ * @param yyyy
+ * @param hhh
+ * @param nnn
+ * @param www
+ * @param in
+ * @return int
+ */
 int BCHDecoder::readInt(int ss, int yyyy, int hhh, int nnn, int www, ByteBuffer& in) {
 	if (ss == 0)
 		return BCH_63_51(concatenate(yyyy, hhh, nnn, www));
 	else
 		return decoder.readInt(in);
 }
-
+/**
+ * @brief concatenate four data into 51 bits that drops one bit from 13 bytes.
+ * @param Y
+ * @param H
+ * @param N
+ * @param W
+ * @return unsigned long long
+ */
 unsigned long long BCHDecoder::concatenate(int Y, int H, int N, int W) {
     unsigned long long res = Y;
     res <<= 11;
@@ -17,6 +33,11 @@ unsigned long long BCHDecoder::concatenate(int Y, int H, int N, int W) {
     res += W;
     return res;
 }
+/**
+ * @brief the implementation of BCH(63, 51, 2) algorithm.
+ * @param origin
+ * @return int
+ */
 int BCHDecoder::BCH_63_51(unsigned long long origin) {
     unsigned long long genPoly = 5433;
     unsigned long long fix = 1;
